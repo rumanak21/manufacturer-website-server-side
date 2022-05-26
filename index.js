3a261f89a7760a2bfa97ebf92e591fd338455693
rumanak21 <rumana2k20@gmail.com>
2022-05-26 14:24:41 +0600

    get product by  id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,8 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.97p5q.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 const uri = `mongodb+srv://electric-tools-manufacturer:${process.env.DB_PASS}@cluster0.ueksz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -22,14 +21,20 @@ async function run() {
         await client.connect();
         const toolsCollection = client.db('electric-tools').collection('tools');
 
-        console.log('toolsCollection')
-
-
         app.get('/tools', async (req, res) => {
             const query = {};
             const cursor = toolsCollection.find(query);
             const tools = await cursor.toArray();
             res.send(tools);
+        });
+
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            console.log(query);
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool);
         });
 
     }
